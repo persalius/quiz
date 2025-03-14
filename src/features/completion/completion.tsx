cd828de97d9cd37df51595490043da34db69e3ea
Shevchenko Sergey <persalius@gmail.com>
2025-03-14 17:19:36 +0200

    Completion

--- src/features/completion/completion.tsx
+++ src/features/completion/completion.tsx
@@ -3,32 +3,37 @@ import { useTranslations } from 'next-intl';
 import { Checkmark } from './components/checkmark';
 import { DownloadIcon } from '@/shared/icons';
 import { Link } from '@/shared/ui/link';
+import { routes } from '@/shared/constants/routes';
 
 export const Completion = () => {
   const t = useTranslations('Completion');
 
   return (
-    <section className="flex flex-col items-center">
-      <Typography
-        variant="h1"
-        className="font-niconne text-regal-grey text-4xl leading-none font-normal"
-      >
-        {t('title')}
-      </Typography>
-      <Typography className="font-albert font-semibold">
-        {t('description')}
-      </Typography>
+    <section className="flex h-full flex-col items-center justify-between pt-24">
+      <div className="flex flex-col items-center">
+        <Typography
+          variant="h1"
+          className="font-niconne text-regal-grey text-4xl leading-none font-normal"
+        >
+          {t('title')}
+        </Typography>
+        <Typography className="font-albert mb-12.5 font-semibold">
+          {t('description')}
+        </Typography>
 
-      <Checkmark />
+        <Checkmark />
+      </div>
 
-      <Typography className="font-albert flex gap-2 font-semibold">
-        <DownloadIcon />
-        {t('download')}
-      </Typography>
+      <div className="flex w-full flex-col items-center gap-7.5">
+        <Typography className="font-albert flex gap-2 font-semibold">
+          <DownloadIcon />
+          {t('download')}
+        </Typography>
 
-      <Link variant="secondary" href="/">
-        Retake quiz
-      </Link>
+        <Link variant="secondary" href={routes.home} className="w-full">
+          {t('retakeQuiz')}
+        </Link>
+      </div>
     </section>
   );
 };

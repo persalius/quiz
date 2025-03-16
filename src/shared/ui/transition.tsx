47487a7e1accc85e3eb6d371dcedec2cd9b5515e
Shevchenko Sergey <persalius@gmail.com>
2025-03-16 14:55:18 +0200

    transition

--- src/shared/ui/transition.tsx
+++ src/shared/ui/transition.tsx
@@ -37,7 +37,7 @@ const variants: Record<AnimationVariants, Variants> = {
     },
   },
   translate: {
-    visible: { opacity: 1, translateY: 0 },
+    visible: { opacity: 1, translateY: 0, transition: { duration: 0.5 } },
     hidden: { opacity: 0, translateY: -50 },
   },
 };
